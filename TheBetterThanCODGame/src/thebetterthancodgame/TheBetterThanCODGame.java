1395dc7e81f5a93a132e63c4f628f15fce4ace7b
WX <rlaterson@gmail.com>
2015-10-10 10:42:52 -0400

    Update TheBetterThanCODGame.java

--- TheBetterThanCODGame/src/thebetterthancodgame/TheBetterThanCODGame.java
+++ TheBetterThanCODGame/src/thebetterthancodgame/TheBetterThanCODGame.java
@@ -10,6 +10,10 @@ package thebetterthancodgame;
  *
  * @author Javier
  */
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -17,12 +21,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 /*from w  w w. ja v  a  2s.c  o m*/
 public class TheBetterThanCODGame extends Application {
   public static void main(String[] args) {
@@ -30,9 +36,9 @@ public class TheBetterThanCODGame extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {
+  public void start(Stage primaryStage) throws UnsupportedEncodingException {
     BorderPane root = new BorderPane();
-    Scene scene = new Scene(root, 380, 750, Color.WHITE);
+    Scene scene = new Scene(root, 780, 750, Color.WHITE);
 
     GridPane gridpane = new GridPane();
     gridpane.setPadding(new Insets(5));
@@ -42,7 +48,16 @@ public class TheBetterThanCODGame extends Application {
     ColumnConstraints column2 = new ColumnConstraints(50, 350, 600);
     column2.setHgrow(Priority.ALWAYS);
     gridpane.getColumnConstraints().addAll(column1, column2);
+    Image image;
 
+    
+     image = new Image("file:C:/res/boss.png");
+   
+    ImageView iv1 = new ImageView();
+    iv1.setPreserveRatio(false);
+    iv1.fitWidthProperty().bind(root.widthProperty());
+    iv1.fitHeightProperty().bind(root.heightProperty());
+    iv1.setImage(image);
     Label fNameLbl = new Label("Console");
     TextField fNameFld = new TextField();
     
@@ -50,7 +65,9 @@ public class TheBetterThanCODGame extends Application {
     Button saveButt = new Button("Enter");
 
     // First name label
-    GridPane.setHalignment(fNameLbl, HPos.RIGHT);
+    GridPane.setHalignment(iv1, HPos.LEFT);
+    gridpane.add(iv1, 0, 0);
+    GridPane.setHalignment(fNameLbl, HPos.LEFT);
     gridpane.add(fNameLbl, 0, 0);
 
     // Last name label
@@ -64,11 +81,15 @@ public class TheBetterThanCODGame extends Application {
  
 
     // Save button
-    GridPane.setHalignment(saveButt, HPos.RIGHT);
+    GridPane.setHalignment(saveButt, HPos.LEFT);
     gridpane.add(saveButt, 1, 2);
-
+     
     root.setCenter(gridpane);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
+
 }
+
+
+
