718717be296bba75b93675f790805251e44e472f
ethachu19 <ethan.mak100@gmail.com>
2015-10-10 17:08:39 -0400

    texture files

--- TheBetterThanCODGame/src/thebetterthancodgame/Enemy.java
+++ TheBetterThanCODGame/src/thebetterthancodgame/Enemy.java
@@ -21,6 +21,8 @@ public class Enemy {
     public String name;
     public int xpGain;
     public int accuracy;
+    public String texNameType;
+    
     public Enemy(double health, int level, EnemyType e, String name, int xpGain, int accuracy){
         this.health = health;
         this.level = level;
@@ -28,6 +30,29 @@ public class Enemy {
         this.xpGain = xpGain;
         this.accuracy = accuracy;
         this.name = name;
+        
+        if(e == EnemyType.BOSS)
+            level = 50;
+        switch(e)
+        {
+            case JUSTINBEAVER:
+                texNameType = "justinbeaver.png";
+                break;
+            case BEAVER:
+                texNameType = "beaver.png";
+                break;
+            case SNEAKYBEAVER:
+                texNameType = "sneakybeaver.png";
+            case SNEAKYBEAKYCT:
+                texNameType = "sneaky beaky ct.png";
+                break;
+            case BLANK:
+                texNameType = null;
+                break;
+            case BOSS:
+                texNameType = "boss.png";
+                break;
+        }
     }
     public String attack(Player p){
         String message = "";
@@ -35,31 +60,39 @@ public class Enemy {
         Random r = new Random();
         int headshotPercent = r.nextInt()%(10-accuracy);
         
-        if(e == EnemyType.JUSTINBEAVER){
-            damage = 3 * level;
-            message = "The Justin Beaver has hit you for ";
+        switch(e)
+        {
+            case JUSTINBEAVER:
+                damage = 3 * level;
+                message = "The Justin Beaver has hit you for ";
+                break;
+            case BEAVER:
+                damage = 2 * level;
+                message = "The Beaver has hit you for ";
+                break;
+            case SNEAKYBEAVER:
+                damage = 2 * level;
+                message = "The Sneaky Beaver has hit you for ";
+            case SNEAKYBEAKYCT:
+                damage = 4 * level;
+                message = "The Sneaky Beaky Counter Terrorist has hit you for ";
+                break;
+            case BLANK:
+                damage = 0;
+                break;
+            case BOSS:
+                damage = 20;
+                message = "The Boss has hit you for ";
+                break;
         }
-        if(e == EnemyType.BEAVER){
-            damage = 2 * level;
-            message = "The Beaver has hit you for ";
-        }
-        if(e == EnemyType.SNEAKYBEAVER){
-            damage = 2 * level;
-            message = "The Sneaky Beaver has hit you for ";
-        }
-        if(e == EnemyType.SNEAKYBEAKYCT){
-            damage = 4 * level;
-            message = "The Sneaky Beaky Counter Terrorist has hit you for ";
-        }
-        if(e == EnemyType.BLANK){
-            damage = 0;
-        }
+        
         if(headshotPercent == 3 - accuracy){
             damage *= 2;
         }
         if(damage == 0){
             return message;
         }
+        
         p.damage(damage);
         return message + damage;
     }
